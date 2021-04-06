6fad154d99cb66283d77f29b37f7bf4b1a2d8d2f
Nick <nizquie0@email.cpcc.edu>
2021-04-05 20:12:30 -0400

    Added user input validation
    Added user input validation

--- Project_Nicolas_Izquierdo.cpp
+++ Project_Nicolas_Izquierdo.cpp
@@ -16,40 +16,46 @@ int main()
 	cout << "How many computers are being processed? ";
     cin >> compNum; //Read in the user's input for the number of computers being processed
     
+    //Validate the user's input
     while(compNum <= 0)
     {
-    	cout << "You've got to process at least 1 computer!";
+    	cout << "You've got to process at least 1 computer!" << endl;
     	cout << "How many computers are being processed? ";
     	cin >> compNum;
 	}
 	
+	cout << "\nComputer Hardware Graphics Quality Recommendation Tool" << endl;
+	
+	//Run the loop for as many times as the user requested
 	for(int num = 1; num <= compNum; num++)
 	{
-    
-    	cout << "\nComputer Hardware Graphics Quality Recommendation Tool" << endl;
-    
     	cout << "\nPlease enter the clock speed (in Megahertz) of your graphics card: ";
     	cin >> gpuSpeed; //Read in the user's input for clock speed of GPU
-    	if(gpuSpeed < 1) //Input validation to confirm the user entered a value
+    	//Input validation to confirm the user entered a value between 800 and 2000
+    	while(gpuSpeed < 800 || gpuSpeed > 2000)
     	{
-    		cout << "Invalid graphics card speed entered. Program will terminate." << endl;
-    		return 0;
+    		cout << "Please enter a number between 800 and 2000!" << endl;
+    		cout << "Please enter the clock speed (in Megahertz) of your graphics card: ";
+    		cin >> gpuSpeed;
 		}
     	
     	cout << "Please enter the clock speed (in Megahertz) of your processor: ";
     	cin >> cpuSpeed; //Read in the user's input for clock speed of CPU
-    	if(cpuSpeed < 1) //Input validation to confirm the user entered a value
+    	//Input validation to confirm the user entered a value between 1000 and 5500
+    	while(cpuSpeed < 1000 || cpuSpeed > 5500)
     	{
-    		cout << "Invalid processor speed entered. Program will terminate." << endl;
-    		return 0;
+    		cout << "Please enter a number between 1000 and 5500!" << endl;
+    		cout << "Please enter the clock speed (in Megahertz) of your processor: ";
+    		cin >> cpuSpeed;
 		}
     	
     	cout << "Please enter the number of cores in your processor: ";
     	cin >> cpuNum; //Read in the user's input for number of cores of CPU
-    	if(cpuNum < 1) //Input validation to confirm the user entered a value
+    	while(cpuNum < 1 || cpuNum > 16) //Input validation to confirm the user entered a value
     	{
-    		cout << "Invalid number of cores entered. Program will terminate." << endl;
-    		return 0;
+    		cout << "Please enter a number between 1 and 16!" << endl;
+    		cout << "Please enter the number of cores in your processor: ";
+    		cin >> cpuNum;
 		}
     	
     	cout << "What is the resolution of your monitor?" << endl;
@@ -59,6 +65,14 @@ int main()
     	cout << "\t 4. 3840 x 2160" << endl;
     	cout << "Please select from the options above: "; //Ask the user to make a choice
     	cin >> select; //Read in the user's input for monitor's resolution
+    	
+    	//Input validation to see if the user choose 1, 2, 3, or 4
+    	while(select < 1 || select > 4)
+    	{
+    		cout << "Please enter 1, 2, 3, or 4!" << endl;
+    		cout << "Please select from the options above: ";
+    		cin >> select;
+		}
     	
     	//Use if-else-if statement to handle the user's selection
     	if (select == 1)
@@ -80,11 +94,6 @@ int main()
 		{           
         	perfScore = ((5 * gpuSpeed) + (cpuNum * cpuSpeed)) * 0.35;
         	res = "3840 x 2160";       
-    	}
-    	else
-		{
-        	cout << "Invalid input. Please enter 1, 2, 3, or 4! Program will terminate.";
-        	return 0; //If the choice was not 1, 2, 3, or 4 then the program ends
     	}
     	
     	//Use if-else-if statement to assign recomended graphics quality to its respective performance score
